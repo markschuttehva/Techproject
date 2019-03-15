bf2373f102a99f3534d119861ef3c52e17630b7b
markschuttehva <45555455+markschuttehva@users.noreply.github.com>
2019-03-16 00:33:21 +0100

    New version

--- index.js
+++ index.js
@@ -1,1 +1,45 @@
-console.log("hello kaas");
+const express = require('express');
+const app = express();
+const port = 9999;
+const db = require('./server/data.js');
+
+app
+    .set('view engine', 'pug')
+    .use('/static', express.static('static'))
+    .get('/', home)
+    .get('/matches', matches)
+    .get('/:id', match)
+    .get('*', error404);
+
+function home(req, res){
+  res.render("home.pug");
+}
+function matches(req, res){
+  res.render("matches.pug");
+}
+function next(req, res){
+  res.render("next.pug");
+}
+function error404(req, res){
+  res.status(404).send('404: De pagina is helaas niet gevonden');
+}
+
+app.listen(port, note())
+function note() {
+  console.log('Server is gestart op port:' + port);
+}
+
+// function matches(req, res) {
+//   res.render('matches.pug', {data: partner})
+// }
+
+function match(req, res) {
+  var id = req.params.id
+  var partner = db.filter(function (value) {
+    return value.id == id
+  })
+  console.log(db);
+  console.log(partner);
+  res.render("matches.pug", { data: partner });
+  res.render("home.pug", { data: partner });
+}
---
+++ server/data.js
@@ -0,0 +1,32 @@
+// Get data from Untappd
+// https://untappd.com/api/docs#userbeers
+
+
+
+
+
+
+//static data
+// This data is temporary until there is an api and database connection
+var data = [
+    {
+        id: 1,
+        name: 'Frans Bauer',
+        age: 45,
+        beer: 'Amstel',
+        totalbeers: 99,
+        city: 'Amsterdam',
+        img: 'https://pbs.twimg.com/profile_images/2409702159/image_400x400.jpg'
+	},
+    {
+        id: 2,
+        name: 'Ali B',
+        age: 40,
+        beer: 'Desparasos',
+        totalbeers: 1,
+        city: 'Amsterdam',
+        img: 'https://www.sevendays.nl/sites/default/files/styles/node_image/public/Ali_B%20%281%29.jpg?itok=s1sKdU2f'
+	}
+];
+
+module.exports = data;
