1ed45cf6f619ac62fdb1b4650376f1e592e374bf
markschuttehva <45555455+markschuttehva@users.noreply.github.com>
2019-06-07 14:57:49 +0200

    Changed some files
    Using more include for some files

--- server/data.js
+++ server/data.js
@@ -9,7 +9,8 @@ const data = [
         city: "Amsterdam",
         img: "https://pbs.twimg.com/profile_images/2409702159/image_400x400.jpg",
         email: "Fransiefrans@gmail.com",
-        password: "lalalala"
+        password: "lalalala",
+        likedusers: ""
 	},
     {
         id: 2,
@@ -19,17 +20,30 @@ const data = [
         city: "Amsterdam",
         img: "https://www.sevendays.nl/sites/default/files/styles/node_image/public/Ali_B%20%281%29.jpg?itok=s1sKdU2f",
         email: "knuffelbeer@gmail.com",
-        password: "1234"
+        password: "1234",
+        likedusers: "",
         },
     {
         id: 3,
-        name: "johan",
+        name: "Ash",
         age: 25,
-        beer: "cola",
-        city: "hogeveen",
-        img: "https://pbs.twimg.com/profile_images/2409702159/image_400x400.jpg",
+        beer: "Cola",
+        city: "Pallettown",
+        img: "https://media.pocketmonsters.net/characters/2.png",
         email: "Fransiefrans@gmail.com",
-        password: "123"
+        password: "123",
+        likedusers: ""
+        },
+    {
+        id: 4,
+        name: "Pika",
+        age: 46,
+        beer: "Dubbelfris",
+        city: "Den Haag",
+        img: "https://mm3.vistoenpantalla.com/imagenes-productos/camiseta-chica-pokemon-pikachu-silueta-large2.jpg",
+        email: "harry@gmail.com",
+        password: "123",
+        likedusers: ""
         }
 ];
 
